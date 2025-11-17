510a02652751e48f5a32f42e531e1174e0297e2b
Yong Hyun <gunyu1019@gmail.com>
2025-11-18 01:42:52 +0900

    Update boj14938.py

--- boj14938.py
+++ boj14938.py
@@ -8,13 +8,15 @@ if __name__ == '__main__':
     distance = [[0x7fffffff for _ in range(n)] for _ in range(n)]
     for i in range(r):
         _a, _b, _c = map(int, sys.stdin.readline().split())
-        distance[_a - 1][_b - 1] = _c
-        distance[_b - 1][_a - 1] = _c
+        distance[_a - 1][_b - 1] = min(_c, distance[_a - 1][_b - 1])
+        distance[_b - 1][_a - 1] = min(_c, distance[_b - 1][_a - 1])
 
     for i in range(n):
         distance[i][i] = 0
-        for j in range(n):
-            for k in range(n):
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
                 distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
 
     result = [0 for _ in range(n)]
