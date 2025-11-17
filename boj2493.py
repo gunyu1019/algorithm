18b23e4cf7519683c336cb4fc7808a35ba90213b
Yong Hyun <gunyu1019@gmail.com>
2025-11-18 01:43:37 +0900

    Optimize solution using deque for stack operations
    Refactor to use a deque for improved efficiency in processing.

--- boj2493.py
+++ boj2493.py
@@ -1,16 +1,18 @@
+import collections
 import sys
 
 
 if __name__ == '__main__':
     n = int(sys.stdin.readline())
     q = list(map(int, sys.stdin.readline().rstrip().split()))
-    r = ["0" for _ in range(n)]
+    r = [0 for _ in range(n)]
+
+    stack = collections.deque()
 
     for i in range(n - 1, -1 , -1):
-        for j in range(i - 1, -1, -1):
-            # print(q[i], q[j], end=" ")
-            if q[i] <= q[j]:
-                r[i] = str(j + 1)
-                break
-        # print()
-    print(" ".join(r))
+        while len(stack) > 0 and stack[-1][0] <= q[i]:
+            r[stack[-1][1]] = i + 1
+            stack.pop()
+        stack.append((q[i], i))
+
+    print(" ".join(map(str, r)))
